84dc510035f5d6d3e88c4f5dcca15674c0b93cc8
Federico Jimenez <federicojimenezpulido@github.com>
2026-04-30 17:47:51 -0500

    Updates

--- assets/AppIsland.-zWWvjrj.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./AppIsland.5WJsP931.js";import"./index.B7uaMGyq.js";export{a as default};
---
+++ assets/AppIsland.oDaxmJeA.js
@@ -0,0 +1,1 @@
+import{A as a}from"./AppIsland.CezglEth.js";import"./index.B7uaMGyq.js";export{a as default};
