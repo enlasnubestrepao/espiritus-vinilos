58cc1860a45957f46e39a61a523628ad0d2527ef
federicojimenezPulido <federicojimenezPulido@users.noreply.github.com>
2026-05-05 15:29:24 +0000

    deploy: 23c4f8e4e040bfa670d896937f59f0c9a377ba59

--- assets/AppIsland.DQPcwAgd.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./AppIsland.jHn4d4Za.js";import"./index.B7uaMGyq.js";export{a as default};
---
+++ assets/AppIsland.W9wgypeb.js
@@ -0,0 +1,1 @@
+import{A as a}from"./AppIsland.B4av4KjG.js";import"./index.B7uaMGyq.js";export{a as default};
